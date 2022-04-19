a3e690eae68f73d155f5b68782fc6fd75c6b08c2
ynn <1830084621@qq.com>
2022-04-19 22:56:38 +0800

    "路由-六个钩子事件"

--- next-create/pages/index.js
+++ next-create/pages/index.js
@@ -24,8 +24,29 @@ import Router from 'next/router';
 //   )
   
 // }
+Router.events.on('routeChangeStart', (...args) => {
+  console.log('1.routeChangeStart->路由开始变化,参数为：', ...args);
+})
+Router.events.on('routeChangeComplete', (...args) => {
+  console.log('2.routeChangeComplete->路由结束变化,参数为：', ...args);
+})
+Router.events.on('beforeHistoryChange', (...args) => {
+  console.log('3.beforeHistoryChange->在改变浏览器history之前触发,参数为：', ...args);
+})
+Router.events.on('routeChangeError', (...args) => {
+  console.log('4.routeChangeError->跳转发生错误,参数为：', ...args);
+})
+Router.events.on('hashChangeStart', (...args) => {
+  console.log('5.hashChangeStart->hash跳转开始时执行,参数为：', ...args);
+})
+Router.events.on('hashChangeComplete', (...args) => {
+  console.log('6.hashChangeComplete->hash跳转完成时,参数为：', ...args);
+})
 
 const Home = () => {
+  
+  console.log(Router.events, 'event')
+
   function gotoStudy() {
     Router.push({
       pathname : '/study',
@@ -42,6 +63,9 @@ const Home = () => {
       </div>
       <div>
         <button onClick={gotoStudy}>pink老师</button>
+      </div>
+      <div>
+        <Link href='#js'><a>学习js</a></Link>
       </div>
     </>
   )
